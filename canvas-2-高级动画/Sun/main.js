742be50851b79119d5dc71ffcc4beeeeb291ee15
Xue <605534423@qq.com>
2019-03-04 20:39:40 +0800

    调整参数

--- canvas-2-高级动画/Sun/main.js
+++ canvas-2-高级动画/Sun/main.js
@@ -1,56 +1,56 @@
-let sun;
-let earth;
-let moon;
-let ctx;
-function init(){
+let sun, earth, moon, ctx;
+
+function init() {
     sun = new Image();
     earth = new Image();
     moon = new Image();
-    sun.src = "img/sun.jpg";
-    earth.src = "img/earth.jpg";
-    moon.src = "img/solar.jpg";
+    sun.src = "img/sun.png";
+    earth.src = "img/earth.png";
+    moon.src = "img/moon.png";
 
-    let canvas = document.querySelector("#solar");
-    ctx = canvas.getContext("2d");
+    let canvas = document.getElementById("solar");
+    if (!canvas.getContext) return;
+    ctx = canvas.getContext('2d');
 
-    moon.onload = function (){
-        draw()
+    sun.onload = function () {
+        draw();
     }
-
 }
+
 init();
-function draw(){
-    ctx.clearRect(0, 0, 300, 300); //清空所有的内容
-    /*绘制 太阳*/
-    ctx.drawImage(sun, 0, 0, 100, 100);
+
+function draw() {
+    ctx.clearRect(0, 0, 600, 300);
+
+    // 绘制太阳
+    ctx.drawImage(sun, 150, 70, 20, 20);
 
     ctx.save();
-    ctx.translate(150, 150);
+    ctx.translate(160, 80);
 
-    //绘制earth轨道
+    // 绘制地球轨道
     ctx.beginPath();
-    ctx.strokeStyle = "rgba(255,255,0,0.5)";
-    ctx.arc(0, 0, 100, 0, 2 * Math.PI);
+    ctx.strokeStyle = "rgba(255, 255, 255, .1)";
+    ctx.arc(0, 0, 50, 0, Math.PI * 2);
     ctx.stroke();
 
     let time = new Date();
-    //绘制地球
-    ctx.rotate(2 * Math.PI / 60 * time.getSeconds() + 2 * Math.PI / 60000 * time.getMilliseconds());
-    ctx.translate(100, 0);
-    ctx.drawImage(earth, -12, -12);
+    // 绘制地球
+    ctx.rotate(2 * Math.PI / 60 * time.getSeconds() + 2 * Math.PI  / 60000 * time.getMilliseconds());
+    ctx.translate(55, 0);
+    ctx.drawImage(earth, -10, -10);
 
-    //绘制月球轨道
+    // 绘制月球
     ctx.beginPath();
-    ctx.strokeStyle = "rgba(255,255,255,.3)";
-    ctx.arc(0, 0, 40, 0, 2 * Math.PI);
+    ctx.strokeStyle = "rgba(255, 255, 255, .2)";
+    ctx.arc(0, 0, 20, 0, 2 * Math.PI);
     ctx.stroke();
 
-    //绘制月球
-    ctx.rotate(2 * Math.PI / 6 * time.getSeconds() + 2 * Math.PI / 6000 * time.getMilliseconds());
-    ctx.translate(40, 0);
-    ctx.drawImage(moon, -3.5, -3.5);
+    ctx.rotate(2 * Math.PI / 6 * time.getSeconds() + 2 * Math.PI  / 6000 * time.getMilliseconds())
+    ctx.translate(20, 20);
+    ctx.drawImage(moon, -12, -12);
     ctx.restore();
 
     requestAnimationFrame(draw);
-}
 
+}
